f4d336b5195cb835239e59945c5e48590b77c1c4
Damoon Robatian <damoon.robatian@gmail.com>
2023-02-24 00:45:43 -0500

    Minor update

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+#%%
 '''
 Text Classification
 
@@ -10,11 +14,6 @@ text classification is nothing but the process in which the text is assigned to 
 text classification classifiers can be used in organizing, structuring and categorizing for much as any type of text. The text from documents, medical studies and files and also all over the web. For this we are going to use Naive bayes classifier which is considered to be good for text classification.
 '''
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 
 
