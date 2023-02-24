2fc9c947df30d53dfe182f346ff238c06dc768a8
Damoon Robatian <damoon.robatian@gmail.com>
2023-02-24 00:34:46 -0500

    Minor update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+data_file = "/home/damoon/Dropbox/teaching/mcgill_teaching/data/amazon_customer_reviews/reviews_may19.csv"
