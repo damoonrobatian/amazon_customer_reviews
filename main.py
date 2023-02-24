0dfb47efe794984cd0582eecc17ea8e4779879c0
Damoon Robatian <damoon.robatian@gmail.com>
2023-02-24 00:36:11 -0500

    Minor update

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+#%% Load data
 data_file = "/home/damoon/Dropbox/teaching/mcgill_teaching/data/amazon_customer_reviews/reviews_may19.csv"
+data = pd.read_csv(data_file)
+data.shape
+data.head()
