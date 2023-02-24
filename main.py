4b6f647c65d21d8cdcf8cccec760fee0cf2e20bd
Damoon Robatian <damoon.robatian@gmail.com>
2023-02-24 01:04:10 -0500

    Minor update

--- main.py
+++ main.py
@@ -22,3 +22,47 @@ data_file = "/home/damoon/Dropbox/teaching/mcgill_teaching/data/amazon_customer_
 data = pd.read_csv(data_file)
 data.shape
 data.head()
+
+
+#%% Replace the text with numbers
+df['new_Labels'] =   df['Labels'].apply(lambda v: 1 if v=='Positive' else 0)
+
+'''
+Here we have created a new column as "new_Labels" which contains the integer values of "Labels" column, for "Positive" we have replaced it with "1" and for "Negative" we have replaced it with "0".
+'''
+df.head()
+df.tail()
+
+#%% Split the data into train and test
+X_train, X_test, y_train, y_test = train_test_split(df['Customer_Reviews'], df['new_Labels'], random_state=1)
+vectorizer = CountVectorizer(strip_accents='ascii', token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*\\b', lowercase=True, stop_words='english')
+X_train_cv = vectorizer.fit_transform(X_train)
+X_test_cv = vectorizer.transform(X_test)
+
+#%% Convert the Customer_Reviews into word count vectors
+Word_frequency = pd.DataFrame(X_train_cv.toarray(), columns=vectorizer.get_feature_names())
+top_words = pd.DataFrame(Word_frequency.sum()).sort_values(0, ascending=False)
+print(Word_frequency, '\n')
+print(top_words)
+
+
+Here in the above we have converted the Reviews into vectors, As the naive bayes classifier needs to be able to calculate how many times each word appears in each document and how many times it appears in each category. for Conversion we have used count vectorizer, and also you can see the word frequency and top words in the above.
+Step 6 - Fit the model and make the predictions
+
+naive_bayes = MultinomialNB()
+naive_bayes.fit(X_train_cv, y_train)
+predictions = naive_bayes.predict(X_test_cv)
+Step 7 - Print the results
+
+print('Accuracy score for Customer Reviews model is: ', accuracy_score(y_test, predictions), '\n')
+print('Precision scorefor Customer Reviews model is: ', precision_score(y_test, predictions), '\n')
+
+Accuracy score for Customer Reviews model is:  0.6666666666666666 
+
+Precision score for Customer Reviews model is:  0.5 
+
+As these are the results based on a sample dataset that only have 10 records, but for more data it will give us more better results. Now we will understand what accuracy and precision score tell us:
+
+Accuracy Score will tell us that out of all the identifications that we have made how many are correct.
+
+Precision Score will tell us that out of all the positive/negative identification we made how many are correct.
